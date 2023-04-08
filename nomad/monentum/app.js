f40ad2aaa1003357d806b8247ed20a9e92d18328
[IoT1]양효빈01~ <yangnorong@gmail.com>
2023-04-08 16:55:09 +0900

    feat:localstorage 저장 후 로딩하는 법 배움

--- nomad/monentum/app.js
+++ nomad/monentum/app.js
@@ -1,17 +1,35 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  console.log(USERNAME_KEY);
+  paintGreetings(username);
 }
 
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+console.log(savedUsername);
+
+if (savedUsername === null) {
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  //show the greetings
+  paintGreetings(savedUsername);
+}
